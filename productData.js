076b26d3842ae73366cec662e9bf812afd16d2e6
Mohamed El-Zarka <53001011+99zarka@users.noreply.github.com>
2023-12-23 00:47:32 +0200

    Add files via upload

---
+++ productData.js
@@ -0,0 +1,224 @@
+productData = [
+    {
+      "title": "برافان معدني 3 ضلفة",
+      "photo": "brafan3.png",
+      "description": [
+        "ابعاد الضلفة  180 سم * 65 سم",
+        "اقصي عرض 195 سم",
+        "ارتفاع 180 سم",
+        "مصنوع من الحديد مطلي الكتروستيتك",
+        "مغطي بقماش blackout"
+      ]
+    },
+    {
+      "title": "سرير كشف",
+      "photo": "examination-bed.jpg",
+      "description": [
+        "ابعاد 195 سم * 58 سم  ارتفاع 65 سم",
+        "مصنوع من الحديد مطلي الكتروستيتك",
+        "اسفنج عالي الكثافة مغطي بالجلد",
+        "الظهر قابل للميل 15, 30, 45 درجة",
+        "سلم 1 درجة 25 * 45 ارتفاع 22 سم",
+        "سلم 2 درجة 40 *45 ارتفاع 43 سم"
+      ]
+    },
+    {
+      "title": "دولاب  معدني 2 ضلفة",
+      "photo": "Cupboard2.png",
+      "description": [
+        "ابعاد 88 *40 ارتفاع 180",
+        "مصنوع من الحديد الصاج  مطلي الكتروستيتك",
+        "4 رف زجاج سيكيورت"
+      ]
+    },
+    {
+      "title": "حامل طبق",
+      "photo": "Cup-holder.png",
+      "description": [
+        "ارتفاع 93 سم",
+        "مصنوع من الاستنلس ستيل 304",
+        "قاعدة خماسية بلاستيكية",
+        "2 طبق استنلس ستيل 304 قطر 31 سم"
+      ]
+    },
+    {
+      "title": "حامل محاليل",
+      "photo": "solution-holder.png",
+      "description": [
+        "ارتفاع 130 : 230 سم",
+        "مصنوع من الاستنلس ستيل 304",
+        "قاعدة خماسية بلاستيكية"
+      ]
+    },
+    {
+      "title": "كرسي طبيب",
+      "photo": "doctor-chair.png",
+      "description": [
+        "ارتفاع 50 : 60 سم",
+        "قرصة قطر 40 سم",
+        "قرصة اسفنج عالي الكثافة مغطي بالجلد",
+        "قاعدة معدنية"
+      ]
+    },
+    {
+      "title": "كرسي الكلي",
+      "photo": "Kidney-chair.png",
+      "description": [
+        "ابعاد 180 * 75 سم",
+        "قابل للارتفاع من 60 الي 85",
+        "مزود بماتور بالإرتفاع وماتور اخر للميل",
+        "مصنوع من حديد مطلي الكتروستيتك"
+      ]
+    },
+    {
+      "title": "سرير كشف نسا",
+      "photo": "women-bed.png",
+      "description": [
+        "ابعاد 180 * 60 سم",
+        "ارتفاع 95 سم",
+        "مصنوع من حديد مطلي الكتروستيتك",
+        "الظهر والرجل قابلين للإرتفاع"
+      ]
+    },
+    {
+      "title": "ترولي",
+      "photo": "trolley.png",
+      "description": [
+        "ابعاد 188 * 55 سم",
+        "قابل للارتفاع من 57 الي 87",
+        "مصنوع من الاستنلس ستيل 304"
+      ]
+    },
+    {
+      "title": "حوض تعقيم",
+      "photo": "sanitation-sink.png",
+      "description": [
+        "ابعاد 94 * 57 سم",
+        "ارتفاع 110 سم",
+        "مصنوع من الاستنلس ستيل 304 تخانة 1.5 مم",
+        "حنفية مزودة photo cell",
+        "مزود بـ dispenser"
+      ]
+    },
+    {
+      "title": "ترابيزة غيار 2 رف",
+      "photo": "table2.png",
+      "description": [
+        "ابعاد 60 * 43 سم",
+        "ارتفاع  83 سم",
+        "مصنوع من الاستنلس ستيل 304",
+        "2 رف استنلس ستيل تخانة 1 مم"
+      ]
+    },
+    {
+      "title": "ترابيزة غيار 3 رف",
+      "photo": "table 3.png",
+      "description": [
+        "ابعاد 60 * 43 سم",
+        "ارتفاع  83 سم",
+        "مصنوع من الاستنلس ستيل 304",
+        "3 رف استنلس ستيل تخانة 1 مم"
+      ]
+    },
+    {
+      "title": "ترولي نقل منســـجوات",
+      "photo": "clothes-trolley.jpg",
+      "description": [
+        "ابعاد 90 * 60 سم",
+        "ارتفاع  80 سم",
+        "مصنوع من الاستنلس ستيل 304",
+        "قماش مقوي مقاوم للبكتريا"
+      ]
+    },
+    {
+      "title": "ترابيزة Over head & Shoes",
+      "photo": "shoe-table.png",
+      "description": [
+        "ابعاد 95 * 28 سم",
+        "ارتفاع  83 سم",
+        "مصنوع من الاستنلس ستيل 304",
+        "2 رف استنلس ستيل تخانة 1 مم"
+      ]
+    },
+    {
+      "title": "Blood Collection Monitor",
+      "photo": "Blood monitor.jpg",
+      "description": [
+        "ابعاد 30 * 26 سم",
+        "ارتفاع  25 سم",
+        "مصنوع من الاستنلس ستيل 304",
+        "يحول الوزن القائم إلى حجم",
+        "يمنع تجلط الدم أثناء التبرع",
+        "التحكم عن طريق : Micro-Controler"
+      ]
+    },
+    {
+      "title": "حوض الآت",
+      "photo": "sink.png",
+      "description": [
+        "مصنوع من الاستنلس ستيل 304",
+        "200*100*50",
+        "300*200*50",
+        "350*250*50",
+        "400*300*50",
+        "500*400*50"
+      ]
+    },
+    {
+      "title": "المكبوسات",
+      "photo": "metal.png",
+      "description": [
+        "مصنوع من الاستنلس ستيل 304",
+        "جفنة اقطار 60, 80, 100, 120, 140, 160, 200, 310, 200 سم",
+        "حوض كلوي 150, 200, 250, 280 مم",
+        "حوض كلوي ثلاثي 150, 200, 250, 280 مم",
+        "قطرميز 100 , 150, 180 مم  قطر القاعدة 50 مم"
+      ]
+    },
+    {
+      "title": "مطرقة عصبية",
+      "photo": "hummer.png",
+      "description": [
+        "مصنوع من الالومنيوم والاستنلس ستيل 304",
+        "بلاستيك ارتليون وبلاستيك اكليرك شفاف"
+      ]
+    },
+    {
+      "title": "ثلاجة حفظ امصال",
+      "photo": "fridge.jpg",
+      "description": [
+        "مصنوع من الأستالنلس ستيل 304"
+      ]
+    }
+  ]
+
+
+let fragment = ''
+
+for ( let product of productData ) {
+    let descriptionHTML = "";
+    for ( let line of product['description'] ){
+        descriptionHTML+=`<li class="list-group-item"> ${line} </li>\n`;
+    }
+
+    let productHTML =
+    `
+    <div class="col-md-4 my-2">
+        <div class="card product-card">
+            <img src="images/${product['photo']}" class="card-img-top" alt=${product['title']}>
+            <div class="card-body">
+                <h5 class="card-title">${product['title']}</h5>
+                <ul class="list-group list-group-flush">
+                    ${descriptionHTML}
+                </ul>
+                <a href="#" class="btn btn-primary d-none">عرض التفاصيل</a>
+            </div>
+        </div>
+    </div>
+    `
+
+    //fragment.appendChild(productHTML);
+    fragment +=productHTML;
+}
+document.getElementById('product-list').innerHTML += fragment;
+
